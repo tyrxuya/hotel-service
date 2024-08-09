f88149dcd41f7b7492af2201b42140502fd5d87a
Ivan Georgiev <ivan2003444@gmail.com>
2024-08-09 11:32:44 +0300

    Rename Jackson Configuration class to JacksonConfig

--- core/src/main/java/com/tinqinacademy/hotel/core/configurations/JacksonConfig.java
+++ core/src/main/java/com/tinqinacademy/hotel/core/configurations/JacksonConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class JacksonConfigure {
+public class JacksonConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
