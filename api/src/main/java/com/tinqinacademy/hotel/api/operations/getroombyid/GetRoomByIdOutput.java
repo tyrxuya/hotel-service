6f40b481e8773bbd2ebd3fd02d5a41af527838b7
Ivan Georgiev <105276674+tyrxuya@users.noreply.github.com>
2024-08-13 11:28:30 +0300

    [HS-007] Fix pom.xml dependencies resolves #18 (#19)
    * Fixed pom.xml files
    
    * Fixed imports due to dependencies

--- api/src/main/java/com/tinqinacademy/hotel/api/operations/getroombyid/GetRoomByIdOutput.java
+++ api/src/main/java/com/tinqinacademy/hotel/api/operations/getroombyid/GetRoomByIdOutput.java
@@ -1,8 +1,8 @@
 package com.tinqinacademy.hotel.api.operations.getroombyid;
 
 import com.tinqinacademy.hotel.api.base.OperationOutput;
-import com.tinqinacademy.hotel.persistence.enums.BathroomType;
-import com.tinqinacademy.hotel.persistence.enums.BedSize;
+import com.tinqinacademy.hotel.api.enums.BathroomType;
+import com.tinqinacademy.hotel.api.enums.BedSize;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
