d3a4a5f2ee96dbf49cd85a998a859c9c6b395f32
Ivan Georgiev <ivan2003444@gmail.com>
2024-08-23 13:18:15 +0300

    Delete unnecessary operations and DTOs

--- api/src/main/java/com/tinqinacademy/hotel/api/operations/createuser/CreateUser.java
+++
@@ -1,6 +0,0 @@
-package com.tinqinacademy.hotel.api.operations.createuser;
-
-import com.tinqinacademy.hotel.api.base.OperationProcessor;
-
-public interface CreateUser extends OperationProcessor<CreateUserInput, CreateUserOutput> {
-}
