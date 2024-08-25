706c6e15180dc2162429503bf40194460e7902c0
Ivan Georgiev <ivan2003444@gmail.com>
2024-08-26 00:09:09 +0300

    Change input/output DTOs

--- api/src/main/java/com/tinqinacademy/hotel/api/operations/hotelvisitor/HotelVisitorOutput.java
+++ api/src/main/java/com/tinqinacademy/hotel/api/operations/hotelvisitor/HotelVisitorOutput.java
@@ -2,6 +2,7 @@ package com.tinqinacademy.hotel.api.operations.hotelvisitor;
 
 import com.tinqinacademy.hotel.api.base.OperationOutput;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -19,15 +20,21 @@ public class HotelVisitorOutput implements OperationOutput {
     @Schema(example = "georgiev")
     private String lastName;
 
-    @Schema(example = "+359889272828")
+    @Schema(example = "0889252012")
     private String phoneNo;
 
-    @Schema(example = "0349888888")
-    private String idNo;
+    @Schema(example = "2003-09-22")
+    private LocalDate birthday;
+
+    @Schema(example = "0349228888")
+    private String civilNumber;
 
     @Schema(example = "mvr varna")
     private String idIssueAuthority;
 
     @Schema(example = "2015-05-22")
     private LocalDate idIssueDate;
+
+    @Schema(example = "2027-12-12")
+    private LocalDate idValidity;
 }
--- api/src/main/java/com/tinqinacademy/hotel/api/operations/unbookroom/UnbookRoomInput.java
+++ api/src/main/java/com/tinqinacademy/hotel/api/operations/unbookroom/UnbookRoomInput.java
@@ -1,7 +1,9 @@
 package com.tinqinacademy.hotel.api.operations.unbookroom;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.tinqinacademy.hotel.api.base.OperationInput;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import org.hibernate.validator.constraints.UUID;
 
@@ -12,7 +14,11 @@ import org.hibernate.validator.constraints.UUID;
 @Getter
 @Setter
 public class UnbookRoomInput implements OperationInput {
-    @Schema(example = "100")
+    @JsonIgnore
     @UUID(message = "bookingId must be a valid UUID")
     private String bookingId;
+
+    @UUID(message = "userId must be a valid UUID")
+    @NotBlank(message = "userId cant be blank")
+    private String userId;
 }
